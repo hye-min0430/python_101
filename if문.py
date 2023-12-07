046c4d96d9deddec69e59ed4336d9713bb59333f
hye-min0430 <152273161+hye-min0430@users.noreply.github.com>
2023-12-07 20:57:31 +0900

    Add files via upload

---
+++ if문.py
@@ -0,0 +1,8 @@
+user = int(input("점수를 입력하세요: "))
+
+if user == 100 :
+    print("축하합니다. 만점입니다.")
+elif 100 > user and user >=70 :
+    print("잘하셨습니다.")
+else :
+    print("열공하세요~")
---
+++ list.py
@@ -0,0 +1,11 @@
+#a=5*3
+#b=5//3
+#비교 연산자 => True or False(boolean type)
+
+#a = 5 < 1
+#print(a)
+
+a = 5!=1
+print(a)
+
+
